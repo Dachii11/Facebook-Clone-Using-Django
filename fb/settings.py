f33049af454b19d582aa05227d5a4840a3231f16
dachii <dachimolodinashvili30@gmail.com>
2023-11-01 15:23:33 +0400

    'added'

--- fb/settings.py
+++ fb/settings.py
@@ -15,7 +15,7 @@ if DEBUG:
     
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = ["https://85cd-2a0d-5600-d-3000-00-a636.ngrok-free.app"]
+CSRF_TRUSTED_ORIGINS = [""]
 # Application definition
 
 INSTALLED_APPS = [
